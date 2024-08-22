13b13fe0df9ff0c0d1f7f0d46a282378aa7b85ec
ri-oz <61837174+ri-oz@users.noreply.github.com>
2024-08-22 11:59:52 +0300

    Update SS_cars_streamlit.py

--- SS_cars_streamlit.py
+++ SS_cars_streamlit.py
@@ -16,18 +16,6 @@ url = 'https://raw.githubusercontent.com/ri-oz/Cars-Analytics/main/CarData.csv'
 
 df_Car = pd.read_csv(url, index_col=0, sep=';')
 
-# 1. Average price and mileage per Model Details and Manuf
-df_avg_price_mileage = df_Car.groupby(['Model Details', 'Manuf'])[['Price', 'Mileage']].mean().reset_index()
-st.dataframe(df_avg_price_mileage)
-
-# 2. Count of Model Details per Manuf
-df_count_model_details = df_Car.groupby('Manuf')['Model Details'].count().reset_index(name='Count of Model Details')
-st.dataframe(df_count_model_details)
-
-# 3. Average year by Model Details
-df_avg_year = df_Car.groupby('Model Details')['Year'].mean().reset_index()
-st.dataframe(df_avg_year)
-
 # Title
 
 st.title('Auto Cenu pārskats Latvijā')
@@ -47,7 +35,17 @@ st.write(df_Car.describe())
 st.header('Sludinājumu dati')
 st.dataframe(df_Car)
 
+# 1. Average price and mileage per Model Details and Manuf
+df_avg_price_mileage = df_Car.groupby(['Model Details', 'Manuf'])[['Price', 'Mileage']].mean().reset_index()
+st.dataframe(df_avg_price_mileage)
 
+# 2. Count of Model Details per Manuf
+df_count_model_details = df_Car.groupby('Manuf')['Model Details'].count().reset_index(name='Count of Model Details')
+st.dataframe(df_count_model_details)
+
+# 3. Average year by Model Details
+df_avg_year = df_Car.groupby('Model Details')['Year'].mean().reset_index()
+st.dataframe(df_avg_year)
 
 
 
